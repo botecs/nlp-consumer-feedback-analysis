633796d46f131e0cec5d967e539ac2ae49ab9d41
Demjan Botond <botecs23@gmail.com>
2026-04-22 22:28:43 +0200

    refactor: project structure

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-def main():
-    print("Hello from nlp-consumer-feedback-analysis!")
+import nltk
 
+def main():
+    # NLTK erőforrások letöltése
+    for res in ['vader_lexicon', 'punkt', 'stopwords', 'wordnet']:
+        nltk.download(res, quiet=True)
+    
+    print("NLTK erőforrások sikeresen letöltve!")
 
 if __name__ == "__main__":
     main()
